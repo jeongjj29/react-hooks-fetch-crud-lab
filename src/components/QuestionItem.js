cc62bdd38da7002dfa1fb9d5d2356761454c4f04
Jae Jeong <jeongjj29@gmail.com>
2024-08-05 15:52:27 -0400

    complete lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,29 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onRemoveQuestion, onAnswerChange }) {
   const { id, prompt, answers, correctIndex } = question;
+
+  function handleDeleteButton() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "Delete",
+    }).then(() => {
+      onRemoveQuestion(id);
+    });
+  }
+
+  function handleDropdown(e) {
+    const newCorrectIndex = e.target.value;
+    console.log(newCorrectIndex);
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        correctIndex: newCorrectIndex,
+      }),
+    })
+      .then((r) => r.json())
+      .then((question) => onAnswerChange(question));
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +37,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleDropdown}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDeleteButton}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, onRemoveQuestion, onAnswerChange }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onRemoveQuestion={onRemoveQuestion}
+            onAnswerChange={onAnswerChange}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
